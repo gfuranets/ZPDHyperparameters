2e317964dd0bc0a535decd60dc873a906b1f37e8
XP20 <67698795+XP20@users.noreply.github.com>
2024-01-22 11:02:07 +0200

    RFCE Fix hparams values

--- ai_evaluators/RFCEvaluator.py
+++ ai_evaluators/RFCEvaluator.py
@@ -3,24 +3,25 @@ from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import f1_score
 
 import ray
+from ray import train
 
 
 class RFCEvaluator(IEvaluator):
     def __init__(self):
         super().__init__()
         self.hyperparameters = {
-            "n_estimators": [[10, 30, 100], (10, 50)],
-            "max_depth": [[1, 10, 20], (0, 20)],
-            "min_samples_split": [[2, 3, 5], (2, 5)],
-            "min_samples_leaf": [[1, 3, 5], (1, 5)]
+            "n_estimators": [[10, 30, 100], (10, 100)],
+            "max_depth": [[1, 10, 20], (1, 30)],
+            "min_samples_split": [[2, 3, 5], (2, 6)],
+            "min_samples_leaf": [[1, 3, 5], (1, 6)]
         }
 
     def evaluate(self, config):
         clf = RandomForestClassifier(
             n_estimators=int(config["n_estimators"]),
             max_depth=int(config["max_depth"]),
-            min_samples_split=config["min_samples_split"],
-            min_samples_leaf=config["min__samples_leaf"],
+            min_samples_split=int(config["min_samples_split"]),
+            min_samples_leaf=int(config["min_samples_leaf"]),
             random_state=TRAINING_RANDOM_STATE
         )
 
@@ -34,6 +35,6 @@ class RFCEvaluator(IEvaluator):
         predictions = clf.predict(X_test)
         f1 = f1_score(y_test, predictions, average='micro')
 
-        return {"f1_score": f1}
+        train.report({"f1_score": f1})
 
 
